a30a135566408aca6ad66fcbf879e8bfe7e73d85
tom_s <t.blokland.1998@gmail.com>
2019-04-03 20:55:00 +0200

    pers rec

--- website/app.py
+++ website/app.py
@@ -41,7 +41,7 @@ def cart():
 
 if __name__ == '__main__':
     mongo_db = mdb_connectie("voordeelshop") # ophalen connectie MongoDB
-    sql_db = connect_sql('voordeelshop_full', 'postgres', 'Welkom01!') # ophalen connectie SQL
+    sql_db = connect_sql('voordeelshop', 'postgres', 'Tom-1998') # ophalen connectie SQL
     #lift(sql_db) # berekenen en invoeren lift
     app.run() # starten applicatie
 
