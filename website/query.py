43488169e29c5a36d352b6ea6f8850905b54a861
vlreinier <43336873+vlreinier@users.noreply.github.com>
2019-04-04 11:59:17 +0200

    timestamp in data transfer en popular products

--- website/query.py
+++ website/query.py
@@ -13,31 +13,29 @@ def sort_big_to_small_on_index_one(id_list):
 
 ## functie voor het ophalen populaire / meest verkochte producten, staat op homepagina
 def popular(sql_connection, mongo_db, sessiondata):
-    total_cartproducts = len(sessiondata)
-    get_similar_products = 1
-    if total_cartproducts in range(1,3):
-        get_similar_products = 5
-    if total_cartproducts in range(3,5):
-        get_similar_products = 2
+    visitor_id = sessiondata['visitor_id']
+    if visitor_id != '':
+        include_personal = True
 
-    personal = []
-    for product_id in sessiondata:
-        product_details = search_sql(sql_connection,"SELECT category, sub_category, gender FROM products WHERE product_id = '{}'".format(product_id))[0]
-        category = product_details[0]
-        sub_category = product_details[1]
-        gender = product_details[2]
-        query_results = search_sql(sql_connection, "SELECT product_id FROM products WHERE category = '{}' AND sub_category = '{}' AND gender= '{}' ORDER BY RANDOM() LIMIT {}".format(category, sub_category, gender, get_similar_products))
-        for tuple in query_results:
-            personal.append(tuple[0])
     popular = []
-    query_results = search_sql(sql_connection, "SELECT product_id, COUNT(*) AS populair FROM orders GROUP BY product_id ORDER BY populair DESC LIMIT 200")
-    for tuple in query_results:
+    popular_orders_last_2months = search_sql(sql_connection, """SELECT orders.product_id, COUNT(*) AS populair FROM sessions
+                                                                INNER JOIN orders ON sessions.session_id = orders.session_id
+                                                                WHERE sessions.session_start > CURRENT_DATE - INTERVAL '2 months'
+                                                                GROUP BY orders.product_id ORDER BY populair DESC LIMIT 150""")
+    popular_viewed_last_month = search_sql(sql_connection, """SELECT viewed_before.product_id FROM viewed_before
+                                                              INNER JOIN visitors ON viewed_before.visitor_id = visitors.visitor_id
+                                                              INNER JOIN buids ON visitors.visitor_id = buids.visitor_id
+                                                              INNER JOIN sessions ON buids.buid = sessions.buid
+                                                              WHERE sessions.session_start > CURRENT_DATE - INTERVAL '1 month'
+                                                              GROUP BY viewed_before.product_id
+                                                              ORDER BY COUNT(viewed_before.product_id) DESC LIMIT 10""")
+    for tuple in popular_orders_last_2months:
         popular.append(tuple[0])
-    if len(personal) < 5:
-        popular = random.sample(popular, 10 - len(personal))
-    else:
-        popular = random.sample(popular, 5)
-    id_list = personal + popular
+    for tuple in popular_viewed_last_month:
+        popular.append(tuple[0])
+
+    popular = random.sample(popular, 5)
+    id_list = popular # + personal
     return get_product_details(mongo_db, id_list, True)
 
 ## functie voor het ophalen van persoonlijke aanbevelingen bezoekersid a.d.h.v. eerder bekeken en gelijke producten
@@ -73,4 +71,4 @@ def loadselected(sql_db, mongo_db, sessiondata):
 ## functie voor het ophalen van soortgelijk product aanbevelingen a.d.h.v. opgeslagen producten
 def selectedsimilar(sql_db,mongo_db,sessiondata):
 	id_list = content_tree(sql_db, sessiondata)
-	return get_product_details(mongo_db, id_list, True)
+	return get_product_details(mongo_db, id_list, False)
